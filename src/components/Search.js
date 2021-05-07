fb09cb7f37a8250069a2096c9d2fa55d58950c77
milkon <melkon_2010@hotmail.com>
2021-05-07 18:30:01 +0200

    testing wikipedia api

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,26 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('');
 
   useEffect(() => {
-    console.log('I RUN IF DATA CHANGES');
+    // const response = (async () => await axios.get(''))();
+
+    const search = async () => {
+      return await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+    };
+    const response = search();
+
+    console.log(response);
   }, [term]);
 
   return (
