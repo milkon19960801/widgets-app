7999cfd5e1dfcdd1fc6497d53e6c0b13059f8e74
milkon <melkon_2010@hotmail.com>
2021-05-07 15:00:07 +0200

    added Accordion component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Accordion from "./components/Accordion";
 
 export default () => {
-  return <h1>Widgets App</h1>;
+  return (
+    <div>
+      <Accordion />
+    </div>
+  );
 };
