2c05b6da626ad54cb93b8d0d3334b512932bd459
milkon <melkon_2010@hotmail.com>
2021-05-07 18:56:22 +0200

    linking search reslut to wikipedia pages

--- src/components/Search.js
+++ src/components/Search.js
@@ -31,9 +31,19 @@ const Search = () => {
   const renderedResults = resutls.map(result => {
     return (
       <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Go
+          </a>
+        </div>
         <div className="content">
           <div className="header">{result.title}</div>
-          {result.snippet}
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
         </div>
       </div>
     );
