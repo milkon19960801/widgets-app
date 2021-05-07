7f8f9339788da5aa185585d830cdaa09b05338c9
milkon <melkon_2010@hotmail.com>
2021-05-07 18:42:49 +0200

    rendereing search result

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,26 +2,42 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('programming');
+  const [resutls, setResults] = useState([]);
 
   useEffect(() => {
     // const response = (async () => await axios.get(''))();
 
     const search = async () => {
-      return await axios.get('https://en.wikipedia.org/w/api.php', {
-        params: {
-          action: 'query',
-          list: 'search',
-          origin: '*',
-          format: 'json',
-          srsearch: term,
-        },
-      });
-    };
-    const response = search();
+      const { data } = await await axios.get(
+        'https://en.wikipedia.org/w/api.php',
+        {
+          params: {
+            action: 'query',
+            list: 'search',
+            origin: '*',
+            format: 'json',
+            srsearch: term,
+          },
+        }
+      );
 
-    console.log(response);
+      setResults(data.query.search);
+    };
+
+    search();
   }, [term]);
+
+  const renderedResults = resutls.map(result => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          {result.snippet}
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -35,6 +51,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
