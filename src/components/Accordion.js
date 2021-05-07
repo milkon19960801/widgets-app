90218034b81e758f59e8267f513602192d5364ec
milkon <melkon_2010@hotmail.com>
2021-05-07 15:09:10 +0200

    creating jsx elements for every item in items array

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const Accordion = ({ items }) => {
-  return <h1>{items.length}</h1>;
+  const renderedItems = items.map(item => {
+    return (
+      <div key={item.title}>
+        <div className="title active">
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </div>
+    );
+  });
+  return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
