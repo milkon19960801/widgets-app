890a6cd960b21a383f3d69356f1b518cd31bf59e
milkon <melkon_2010@hotmail.com>
2021-05-08 16:16:11 +0200

    added input form

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import Translate from './components/Translate';
 const items = [
   {
     title: 'What is React?',
-    content: 'React is a fron end javaScript framework',
+    content: 'React is a front end javascript framework',
   },
   {
     title: 'Why use React?',
-    content: 'React is a favorite javaScript library among engineers',
+    content: 'React is a favorite JS library among engineers',
   },
   {
     title: 'How do you use React?',
--- src/components/Translate.js
+++ src/components/Translate.js
@@ -3,25 +3,38 @@ import Dropdown from './Dropdown';
 
 const options = [
   {
-    lable: 'Afrikaans',
+    label: 'Afrikaans',
     value: 'af',
   },
   {
-    lable: 'Arabic',
+    label: 'Arabic',
     value: 'ar',
   },
   {
-    lable: 'Hindi',
+    label: 'Hindi',
     value: 'hi',
   },
 ];
 
 const Translate = () => {
   const [language, setLanguage] = useState(options[0]);
+  const [text, setText] = useState('');
 
   return (
     <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Text</label>
+          <input
+            value={text}
+            onChange={e => {
+              setText(e.target.value);
+            }}
+          />
+        </div>
+      </div>
       <Dropdown
+        label="Select a Language"
         selected={language}
         onSelectedChange={setLanguage}
         options={options}
