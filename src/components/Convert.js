7fda49528a4b10e493c487a570d9881083c67ee3
milkon <melkon_2010@hotmail.com>
2021-05-08 17:23:19 +0200

    added Convert component

---
+++ src/components/Convert.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+const Convert = ({ language, text }) => {
+  useEffect(() => {
+    console.log('New language or text');
+  }, [language, text]);
+
+  return <div />;
+};
+
+export default Convert;
--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Dropdown from './Dropdown';
+import Convert from './Convert';
 
 const options = [
   {
@@ -39,6 +40,9 @@ const Translate = () => {
         onSelectedChange={setLanguage}
         options={options}
       />
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert text={text} language={language} />
     </div>
   );
 };
