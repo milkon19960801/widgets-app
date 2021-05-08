bad227205ae62d468744ad6685c1ceacb0ae2da2
milkon <melkon_2010@hotmail.com>
2021-05-08 12:10:22 +0200

    fixing doubel api request bug

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,18 @@ import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('programming');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [resutls, setResults] = useState([]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
   useEffect(() => {
     const search = async () => {
@@ -15,26 +26,14 @@ const Search = () => {
             list: 'search',
             origin: '*',
             format: 'json',
-            srsearch: term,
+            srsearch: debouncedTerm,
           },
         }
       );
       setResults(data.query.search);
     };
-
-    if (term && !resutls.length) search();
-    else {
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          search();
-        }
-      }, 1000);
-
-      return () => {
-        clearTimeout(timeoutId);
-      };
-    }
-  }, [term]);
+    search();
+  }, [debouncedTerm]);
 
   const renderedResults = resutls.map(result => {
     return (
