54b23d4ee91e475ede6b6955d4aa80299c24fb57
milkon <melkon_2010@hotmail.com>
2021-05-08 14:13:20 +0200

    fixed event bubling issue

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    document.body.addEventListener(
+      'click',
+      event => {
+        if (ref.current.contains(event.target)) return;
+        setOpen(false);
+      },
+      { capture: true }
+    );
+  }, []);
 
   const rederedOptions = options.map(option => {
     if (option.value === selected.value) return null;
-
     return (
       <div
         key={option.value}
@@ -18,12 +29,15 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
       </div>
     );
   });
+
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
         <div
-          onClick={() => setOpen(!open)}
+          onClick={() => {
+            setOpen(!open);
+          }}
           className={`ui selection dropdown ${open ? 'visible active' : ''}`}
         >
           <i className="dropdown icon"></i>
