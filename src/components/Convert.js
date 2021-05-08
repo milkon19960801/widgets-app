57878c9bd0170cf8b5b4986552245471d1351e43
milkon <melkon_2010@hotmail.com>
2021-05-08 17:38:41 +0200

    rendering translated text

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -2,22 +2,31 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState('');
   useEffect(() => {
-    axios.post(
-      'https://translation.googleapis.com/language/translate/v2',
-      {},
-      {
-        params: {
-          accept: 'text/plain',
-          q: text,
-          target: language.value,
-          key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
-        },
-      }
-    );
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        'https://translation.googleapis.com/language/translate/v2',
+        {},
+        {
+          params: {
+            accept: 'text/plain',
+            q: text,
+            target: language.value,
+            key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
+          },
+        }
+      );
+      setTranslated(data.data.translations[0].translatedText);
+    };
+    doTranslation();
   }, [language, text]);
 
-  return <div />;
+  return (
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
+  );
 };
 
 export default Convert;
