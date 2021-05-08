79b63ea6a1c56d68e95189585ffb389acf1198f2
milkon <melkon_2010@hotmail.com>
2021-05-08 12:27:25 +0200

    added Dropdown component with options array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
   {
@@ -17,10 +18,25 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red',
+  },
+  {
+    label: 'The Color Green',
+    value: 'green',
+  },
+  {
+    label: 'A Shade of Blue',
+    value: 'blue',
+  },
+];
+
 export default () => {
   return (
     <div>
-      <Search />
+      <Dropdown options={options} />
     </div>
   );
 };
---
+++ src/components/Dropdown.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Dropdown = () => {
+  return <h1>Dropdown</h1>;
+};
+
+export default Dropdown;
