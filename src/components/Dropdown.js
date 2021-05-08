376eac1ea67e6716fab4d2bea7c968b40399fc5e
milkon <melkon_2010@hotmail.com>
2021-05-08 12:35:00 +0200

    added some JSX to dropdown component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
-const Dropdown = () => {
-  return <h1>Dropdown</h1>;
+const Dropdown = ({ options }) => {
+  const rederedOptions = options.map(option => {
+    return (
+      <div key={option.value} className="item">
+        {option.label}
+      </div>
+    );
+  });
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label className="label">Select a Color</label>
+        <div className="ui selection dropdown visible active">
+          <i className="dropdown icon"></i>
+          <div className="text">Select Color</div>
+          <div className="menu visible transition">{rederedOptions}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dropdown;
