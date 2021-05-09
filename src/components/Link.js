0438580535c470b60a30098d2cb5f6b1370561e6
milkon <melkon_2010@hotmail.com>
2021-05-09 21:37:52 +0200

    updating the Route

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Link = ({ className, href, children }) => {
   const onClick = event => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
     event.preventDefault();
     window.history.pushState({}, '', href);
+
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
   };
 
   return (
