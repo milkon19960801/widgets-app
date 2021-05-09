38a5b9f272e744f7018385bad9719c6606d73eb1
milkon <melkon_2010@hotmail.com>
2021-05-09 20:57:20 +0200

    code clean

--- src/App.js
+++ src/App.js
@@ -37,34 +37,6 @@ const options = [
   },
 ];
 
-const showAccordion = () => {
-  if (window.location.pathname === '/') {
-    return <Accordion items={items} />;
-  }
-};
-
-const showList = () => {
-  if (window.location.pathname === '/list') {
-    return <Search />;
-  }
-};
-
-const showDropdown = () => {
-  if (window.location.pathname === '/dropdown') {
-    return <Dropdown />;
-  }
-};
-
-const showTranslate = () => {
-  if (window.location.pathname === '/translate') {
-    return <Translate />;
-  }
-};
-
-const showComponent = (route, component) => {
-  return window.location.pathname === route ? component : null;
-};
-
 export default () => {
   const [selected, setSelected] = useState(options[0]);
 
