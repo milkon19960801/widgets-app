a4fc8e8df2ffe0b609e2b809d69f892cef9cba87
milkon <melkon_2010@hotmail.com>
2021-05-09 21:22:50 +0200

    added some props to link component

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const Link = () => {
-  return <a>Link</a>;
+const Link = ({ className, href, children }) => {
+  const onClick = event => {
+    event.preventDefault();
+  };
+
+  return (
+    <a onClick={onClick} className={className} href={href}>
+      {children}
+    </a>
+  );
 };
 
 export default Link;
