e1870b91e1699db32d0a0582e1edae6a5dcbc6eb
milkon <melkon_2010@hotmail.com>
2021-05-09 20:53:41 +0200

    added Route component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+/* eslint-disable import/no-anonymous-default-export */
 import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
 
 const items = [
   {
@@ -58,13 +60,35 @@ const showTranslate = () => {
   }
 };
 
+const showComponent = (route, component) => {
+  return window.location.pathname === route ? component : null;
+};
+
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      {showAccordion()}
-      {showList()}
-      {showDropdown()}
-      {showTranslate()}
+      <Route path={'/'}>
+        <Accordion items={items} />
+      </Route>
+
+      <Route path={'/list'}>
+        <Search />
+      </Route>
+
+      <Route path={'/dropdown'}>
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+
+      <Route path={'/translate'}>
+        <Translate />
+      </Route>
     </div>
   );
 };
