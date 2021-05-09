3ca8776a03ba64bd0281e528cf2555a723a1cb62
milkon <melkon_2010@hotmail.com>
2021-05-09 20:45:40 +0200

    basic component routing

--- src/App.js
+++ src/App.js
@@ -34,10 +34,37 @@ const options = [
   },
 ];
 
+const showAccordion = () => {
+  if (window.location.pathname === '/') {
+    return <Accordion items={items} />;
+  }
+};
+
+const showList = () => {
+  if (window.location.pathname === '/list') {
+    return <Search />;
+  }
+};
+
+const showDropdown = () => {
+  if (window.location.pathname === '/dropdown') {
+    return <Dropdown />;
+  }
+};
+
+const showTranslate = () => {
+  if (window.location.pathname === '/translate') {
+    return <Translate />;
+  }
+};
+
 export default () => {
   return (
     <div>
-      <Translate />
+      {showAccordion()}
+      {showList()}
+      {showDropdown()}
+      {showTranslate()}
     </div>
   );
 };
